6a12f2f0f9b69c8d3f9e0c2c45011d95d5d1ac28
Sahar Sistani <saharsistani137777@gmail.com>
2026-01-02 22:11:48 +0330

    gnn_model.py

--- gnn_model.py
+++ gnn_model.py
@@ -1,1 +1,19 @@
+import torch
+import torch.nn as nn
+from torch_geometric.nn import GCNConv
+
+class FakeNewsGNN(nn.Module):
+    def __init__(self, in_channels, hidden_channels=64, out_channels=2):
+        super(FakeNewsGNN, self).__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+    
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.conv2(x, edge_index)
+        return x
 
